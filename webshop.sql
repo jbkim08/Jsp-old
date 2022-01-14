b7648b41b7a3b4c264b64ea4b2c915576eacc3a8
kjb <drv98@naver.com>
2022-01-14 12:28:13 +0900

    1월14일오전

---
+++ webshop.sql
@@ -0,0 +1,13 @@
+# 데이이터베이스 웹샵이 없으면 새로 생성 uft8mb4 한글 뿐만아니라 이모지 포함한 문자 가능
+create database if not exists webshop 
+default character set utf8mb4 collate utf8mb4_unicode_ci;
+
+use webshop; # 웹샵을 선택
+
+# 테이블 유저가 있으면 삭제하고 새로 만듬
+DROP TABLE IF exists user;
+create table user (
+	id int primary key auto_increment,  # 유저 아이디 자동생성
+    email varchar(50), 					# 이메일
+    password varchar(50)				# 비번
+);
